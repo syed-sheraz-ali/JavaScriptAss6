bdb472cae18a89c916de1f4e774f715636189bb1
syed sheraz ali <55004534+syed-sheraz-ali@users.noreply.github.com>
2020-06-24 00:30:42 +0500

    Tasks done

---
+++ chapter 21-25.js
@@ -0,0 +1,31 @@
+//Task 1 
+
+var firstName = prompt("Enter your first name ");
+var LastName = prompt("Enter your last name");
+ var fullName = firstName + " " + LastName 
+
+ alert("Hello and Good Luck " + fullName);
+
+//TASk 2 
+
+var yourfav = prompt("Enter about your favourite phone")
+var l = yourfav.length;
+document.write("Your favourite phoe is " + yourfav + "<br/>");
+
+document.write("length of the string is : " + l );
+
+
+//TASK 3
+var str = "Pakistani"
+var n = str.indexOf("n")
+document.write("<br/>Index of 'n' :" + n)
+
+//TASK 4
+var str1 = "Hello World"
+var s = str1.lastIndexOf("l")
+document.write("<br/>Index of 'l' :" + s)
+
+//TASK 5 
+var str2 = "Pakistan"
+var a = str2.charAt(3)
+document.write("<br/> " + a)
