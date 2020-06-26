ee1ab196d7346baa4244b44adeadc34a69134783
syed sheraz ali <55004534+syed-sheraz-ali@users.noreply.github.com>
2020-06-26 21:40:13 +0500

    Update chapters31-34.js

--- chapters31-34/chapters31-34.js
+++ chapters31-34/chapters31-34.js
@@ -1,134 +1,134 @@
-// //TASK 1
+//TASK 1
 
-// var rightNow = new Date();
+var rightNow = new Date();
 
-// document.write(rightNow);
+document.write(rightNow);
 
-// //TASK 2
+//TASK 2
 
-// var date = new Date();
+var date = new Date();
 
-// var month = new Array();
-// month[0] = "January";
-// month[1] = "February";
-// month[2] = "March";
-// month[3] = "April";
-// month[4] = "May";
-// month[5] = "June";
-// month[6] = "July";
-// month[7] = "August";
-// month[8] = "September";
-// month[9] = "October";
-// month[10] = "November";
-// month[11] = "December";
-// var m = month[date.getMonth()];
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var m = month[date.getMonth()];
 
-// document.write("<br/> Current Month is : " + m);
+document.write("<br/> Current Month is : " + m);
 
-// //TASK 3
-// var date1 = new Date();
-// var Day = new Array();
-// Day[0] = "Sunday";
-// Day[1] = "Monday";
-// Day[2] = "Tuesday";
-// Day[3] = "Wednesday";
-// Day[4] = "Thursday";
-// Day[5] = "Friday";
-// Day[6] = "Saturday";
+//TASK 3
+var date1 = new Date();
+var Day = new Array();
+Day[0] = "Sunday";
+Day[1] = "Monday";
+Day[2] = "Tuesday";
+Day[3] = "Wednesday";
+Day[4] = "Thursday";
+Day[5] = "Friday";
+Day[6] = "Saturday";
 
-// var d = Day[date1.getDay()];
-// document.write("<br/> Today is : " + d.slice(0, 3));
+var d = Day[date1.getDay()];
+document.write("<br/> Today is : " + d.slice(0, 3));
 
-// //TASK 4
-// var date2 = new Date();
-// var Day2 = new Array();
-// Day2[0] = "Sunday";
-// Day2[1] = "Monday";
-// Day2[2] = "Tuesday";
-// Day2[3] = "Wednesday";
-// Day2[4] = "Thursday";
-// Day2[5] = "Friday";
-// Day2[6] = "Saturday";
+//TASK 4
+var date2 = new Date();
+var Day2 = new Array();
+Day2[0] = "Sunday";
+Day2[1] = "Monday";
+Day2[2] = "Tuesday";
+Day2[3] = "Wednesday";
+Day2[4] = "Thursday";
+Day2[5] = "Friday";
+Day2[6] = "Saturday";
 
-// var d1 = Day2[date2.getDay()];
-// if (d1 === "Sunday" || d1 === "Saturday") {
-//   document.write("</br>Today is fun day ");
-// } else {
-//   document.write("</br>Today is : " + d1);
-// }
+var d1 = Day2[date2.getDay()];
+if (d1 === "Sunday" || d1 === "Saturday") {
+  document.write("</br>Today is fun day ");
+} else {
+  document.write("</br>Today is : " + d1);
+}
 
-// //TASK 5
-// var date3 = new Date();
-// var day = date3.getDate();
-// document.write("<br/> " + day);
+//TASK 5
+var date3 = new Date();
+var day = date3.getDate();
+document.write("<br/> " + day);
 
-// if (day <= 15) {
-//   document.write("<br/> First fifteen days of the month ");
-// } else {
-//   document.write("<br/>Last days of the month ");
-// }
+if (day <= 15) {
+  document.write("<br/> First fifteen days of the month ");
+} else {
+  document.write("<br/>Last days of the month ");
+}
 
-// //TASK 6
-// var calculate = new Date(prompt("enter your date of birth", "dec 11 , 1920"));
-// var calculatemili = calculate.getTime();
-// var today = new Date();
-// var todaymili = today.getTime();
-// var diff = todaymili - calculatemili;
-// var accuage = Math.floor(diff / (24 * 30 * 12));
-// document.write("<br/>Current Date :" + today);
+//TASK 6
+var calculate = new Date(prompt("enter your date of birth", "dec 11 , 1920"));
+var calculatemili = calculate.getTime();
+var today = new Date();
+var todaymili = today.getTime();
+var diff = todaymili - calculatemili;
+var accuage = Math.floor(diff / (24 * 30 * 12));
+document.write("<br/>Current Date :" + today);
 
-// document.write(
-//   "<br/>Elapsed miliseconds sicnce  :" + calculate + " is : " + diff
-// );
-// document.write(
-//   "<br/>Elapsed miliseconds sicnce  :" + calculate + " is : " + accuage
-// );
+document.write(
+  "<br/>Elapsed miliseconds sicnce  :" + calculate + " is : " + diff
+);
+document.write(
+  "<br/>Elapsed miliseconds sicnce  :" + calculate + " is : " + accuage
+);
 
-// //TAK 7
-// var time = date.getHours();
-// var minutes = date.getMinutes();
-// document.write("<br/> " + time + " : " + minutes);
-// if (time < 12) {
-//   document.write("<br/> IT's AM");
-// } else {
-//   document.write("<br/> IT's PM");
-// }
+//TAK 7
+var time = date.getHours();
+var minutes = date.getMinutes();
+document.write("<br/> " + time + " : " + minutes);
+if (time < 12) {
+  document.write("<br/> IT's AM");
+} else {
+  document.write("<br/> IT's PM");
+}
 
-// //TASK 8
-// var laterDate = new Date(2020, 11, 31, 23, 59);
-// document.write("<br/>Later Date : " + laterDate);
+//TASK 8
+var laterDate = new Date(2020, 11, 31, 23, 59);
+document.write("<br/>Later Date : " + laterDate);
 
-// //TASK 9
+//TASK 9
 
-// var date1 = new Date();
-// var date2 = new Date("2020 , 04 , 25");
+var date1 = new Date();
+var date2 = new Date("2020 , 04 , 25");
 
-// var Difference_In_Time = date2.getTime() - date1.getTime();
+var Difference_In_Time = date2.getTime() - date1.getTime();
 
-// var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
-// Difference_In_Days = Math.ceil(Difference_In_Days);
-// Difference_In_Days = Difference_In_Days * -1;
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+Difference_In_Days = Math.ceil(Difference_In_Days);
+Difference_In_Days = Difference_In_Days * -1;
 
-// document.write(
-//   "Total number of days between dates  <br>" +
-//     date1 +
-//     "<br> and <br>" +
-//     date2 +
-//     " is: <br> " +
-//     Difference_In_Days +
-//     " days have passed since Ramadan"
-// );
+document.write(
+  "Total number of days between dates  <br>" +
+    date1 +
+    "<br> and <br>" +
+    date2 +
+    " is: <br> " +
+    Difference_In_Days +
+    " days have passed since Ramadan"
+);
 
-// //TASK 10 
+//TASK 10 
 
-// var refDate = new Date(2020 , 04 , 01);
-// var today = new Date();
-// var diff = today - refDate ; 
+var refDate = new Date(2020 , 04 , 01);
+var today = new Date();
+var diff = today - refDate ; 
 
-// diff / 1000;
+diff / 1000;
 
-// var sec = Math.round(diff);
-// document.write("<br/> Time elapse in seconds since " + refDate +"= : " + diff + 'seconds')
+var sec = Math.round(diff);
+document.write("<br/> Time elapse in seconds since " + refDate +"= : " + diff + 'seconds')
 
 
 
@@ -140,20 +140,20 @@ document.write("<br/> Now time is  : " + d7)
 d7.setHours(d7.getHours() - 1);
 document.write("<br/> Time 1 hour before is   : " + d7)
 
-// //TASK 12
+//TASK 12
 
-// var d6 = new Date();
-// document.write("<br/> Today is : " + d6)
-// d6.setFullYear(d6.getFullYear() - 100);
-// document.write("<br/> Date 100 years before is : " + d6)
+var d6 = new Date();
+document.write("<br/> Today is : " + d6)
+d6.setFullYear(d6.getFullYear() - 100);
+document.write("<br/> Date 100 years before is : " + d6)
 
-// //TASK 13 
-//  var age = +prompt("Enter your age ")
-//  var thr = new Date();
-//  var thr1 = thr.getFullYear() 
-//  var year = thr1 - age 
+//TASK 13 
+ var age = +prompt("Enter your age ")
+ var thr = new Date();
+ var thr1 = thr.getFullYear() 
+ var year = thr1 - age 
  
-//  document.write("<br/>Your birth year is  " +year)
+ document.write("<br/>Your birth year is  " +year)
 
 
  //TASK 14 
